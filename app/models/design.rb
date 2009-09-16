0aadafe3d3d1b89ce73607c49d4d9f889fd89ae0
Michael Maloney <michael@michael-maloneys-imac.local>
2009-09-16 12:52:30 -0400

    changed imagemagick command to crop/resize for all images

--- app/models/design.rb
+++ app/models/design.rb
@@ -1,7 +1,7 @@
 class Design < ActiveRecord::Base
   belongs_to :user
   
-  has_attached_file :header_image, :styles => { :header_format => "710x100" }
+  has_attached_file :header_image, :styles => { :header_format => "710x100#" }
   
   # Virtual attribute from user.design_type
   def design_type
--- app/models/piece.rb
+++ app/models/piece.rb
@@ -5,11 +5,11 @@ class Piece < ActiveRecord::Base
   validates_presence_of :title
   validates_presence_of :price, :if => :for_sale?
   
-  has_attached_file :image_1, :styles => { :small_thumb => "70x70>", :large_thumb => "116x116>", :full_size => "710x300>" }
-  has_attached_file :image_2, :styles => { :small_thumb => "70x70", :large_thumb => "116x116", :full_size => "710x300" }
-  has_attached_file :image_3, :styles => { :small_thumb => "70x70", :large_thumb => "116x116", :full_size => "710x300" }
-  has_attached_file :image_4, :styles => { :small_thumb => "70x70>", :large_thumb => "116x116>", :full_size => "710x300>" }
-  has_attached_file :image_5, :styles => { :small_thumb => "70x70", :large_thumb => "116x116", :full_size => "710x300" }
+  has_attached_file :image_1, :styles => { :small_thumb => "70x70#", :large_thumb => "116x116#", :full_size => "710x300#" }
+  has_attached_file :image_2, :styles => { :small_thumb => "70x70#", :large_thumb => "116x116#", :full_size => "710x300#" }
+  has_attached_file :image_3, :styles => { :small_thumb => "70x70#", :large_thumb => "116x116#", :full_size => "710x300#" }
+  has_attached_file :image_4, :styles => { :small_thumb => "70x70#", :large_thumb => "116x116#", :full_size => "710x300#" }
+  has_attached_file :image_5, :styles => { :small_thumb => "70x70#", :large_thumb => "116x116#", :full_size => "710x300#" }
   
   def display_price
     "$" + self.price.to_s unless !self.for_sale
