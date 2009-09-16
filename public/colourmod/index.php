7e16226e12793290878eab88054383e1ef1406d3
Michael Maloney <michael@michael-maloneys-imac.local>
2009-09-16 12:49:54 -0400

    adding colourmod to public

---
+++ public/colourmod/index.php
@@ -0,0 +1,29 @@
+<!--
+
+	ColourMod Plug-N-Play v2.2
+	DHTML Dynamic Color Picker/Selector
+	© 2005 ColourMod.com
+	Design/Programming By Stephen Hallgren (www.teevio.net)
+	Produced By The Noah Institute (www.noahinstitute.org)
+	Manual: http://colourmod.com/blog/?page_id=37
+	
+-->
+
+<div id="cmDefault">
+	<div id="cmColorContainer" class="cmColorContainer"></div>
+	<div id="cmSatValBg" class="cmSatValBg"></div>
+	<div id="cmDefaultMiniOverlay" class="cmDefaultMiniOverlay"></div>
+	<div id="cmSatValContainer">
+		<div id="cmBlueDot" class="cmBlueDot"></div>
+	</div>
+	<div id="cmHueContainer">
+		<div id="cmBlueArrow" class="cmBlueArrow"></div>
+	</div>
+	<div id="cmClose">
+		<input type="text" name="cmHex" id="cmHex" value="FFFFFF" maxlength="6" size="9" /> <a href="http://www.colourmod.com" id="cmCloseButton" ><img src="/colourmod/images/close.gif" border="0" alt="Close ColourMod" /></a>
+	</div>
+	<div style="display:none">
+		<input type="text" name="cmHue" id="cmHue" value="0" maxlength="3" />
+	</div>
+	<a href="http://www.colourmod.com" target="_blank" title="ColourMod - Dynamic Color Picker" class="cmLink">&copy; ColourMod.com</a>
+</div>
