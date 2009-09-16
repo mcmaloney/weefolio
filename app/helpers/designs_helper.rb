5da8593e05db765d329ee5555b9bbe5e28f97f1a
Michael Maloney <michael@michael-maloneys-imac.local>
2009-09-16 13:00:34 -0400

    refactoring edit design view

--- app/helpers/designs_helper.rb
+++ app/helpers/designs_helper.rb
@@ -1,2 +1,9 @@
 module DesignsHelper
+  def css_link(user)
+    if user.design_type == 1
+      link_to 'Edit Custom CSS', :controller => 'users', :action => 'switch_design_type', :id => @user.id
+    else
+      link_to 'Edit Form CSS', :controller => 'users', :action => 'switch_design_type', :id => @user.id
+    end
+  end
 end
