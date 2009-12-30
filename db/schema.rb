2034a796773fe01238f77e6bd947301626f3660a
Michael Maloney <michael@michael-maloneys-imac.local>
2009-12-30 17:06:32 -0500

    changes to weefolio view

--- db/schema.rb
+++ db/schema.rb
@@ -28,30 +28,30 @@ ActiveRecord::Schema.define(:version => 20091029045433) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "background_color",          :default => "#dedddb"
+    t.string   "background_color",          :default => "f5f5f3"
     t.string   "background_align",          :default => "center"
     t.string   "background_tile",           :default => "repeat"
-    t.string   "page_bg_color",             :default => "#fff"
-    t.string   "link_color",                :default => "#72b225"
-    t.string   "link_hover_color",          :default => "#242420"
-    t.string   "link_img_border_color",     :default => "#f5f4f4"
-    t.string   "text_color",                :default => "#767574"
+    t.string   "page_bg_color",             :default => "fff"
+    t.string   "link_color",                :default => "72b225"
+    t.string   "link_hover_color",          :default => "242420"
+    t.string   "link_img_border_color",     :default => "f5f4f4"
+    t.string   "text_color",                :default => "767574"
     t.integer  "text_size",                 :default => 12
     t.string   "text_font_1",               :default => "Arial"
     t.string   "text_font_2",               :default => "Helvetica"
     t.boolean  "text_serif",                :default => false
-    t.string   "title_color",               :default => "#242420"
+    t.string   "title_color",               :default => "242420"
     t.boolean  "title_bold",                :default => false
     t.boolean  "title_italic",              :default => false
     t.string   "title_font",                :default => "Georgia"
     t.boolean  "title_serif",               :default => true
     t.integer  "tagline_size"
-    t.string   "tagline_color",             :default => "#242420"
+    t.string   "tagline_color",             :default => "72b225"
     t.boolean  "tagline_bold",              :default => false
     t.boolean  "tagline_italic",            :default => false
     t.string   "tagline_font",              :default => "Georgia"
     t.boolean  "tagline_serif",             :default => true
-    t.string   "border_color",              :default => "#eee"
+    t.string   "border_color",              :default => "eee"
     t.text     "css_text"
     t.string   "header_image_file_name"
     t.string   "header_image_content_type"
