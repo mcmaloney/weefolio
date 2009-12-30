2ea48596aa8046aa75c9428920628c7724e74982
Michael Maloney <michael@michael-maloneys-imac.local>
2009-12-30 17:08:54 -0500

    changing login redirect

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
       self.current_user = user
       new_cookie_flag = (params[:remember_me] == "1")
       handle_remember_cookie! new_cookie_flag
-      redirect_back_or_default('/')
+      redirect_to edit_user_portfolio_path(current_user, current_user.portfolio)
       flash[:notice] = "Hello, <strong>#{current_user.login}</strong>!"
     else
       note_failed_signin
