8de103ce93d82895325ee3de01e91f80fe2f17a7
Michael Maloney <michael@michael-maloneys-imac.local>
2010-01-03 17:31:11 -0500

    changing an error message around

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -64,7 +64,7 @@ class UsersController < ApplicationController
       redirect_to root_path
       flash[:notice] = "Welcome to Weefolio, #{@user.login}!"
     else
-      flash[:error]  = "We couldn't set up that account, sorry.  Please try again, or contact an admin (link is above)."
+      flash[:error]  = "We couldn't set up that account, sorry.  Please try again, or #{link_to 'contact us', contact_path}."
       render :action => 'new'
     end
   end
