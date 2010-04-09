94f25f1bb37b86f550e9c2f2cbf811f6179ec822
Michael Maloney <michaelmaloney@Crush-Air.local>
2010-04-09 17:18:23 -0400

    screw it. no caching.

--- app/controllers/main_pages_controller.rb
+++ app/controllers/main_pages_controller.rb
@@ -1,6 +1,5 @@
 class MainPagesController < ApplicationController
   layout "template"
-  caches_page :home, :terms_of_use, :privacy_policy
   
   def home
     @page_title = "Weefolio - Lil' portfolios for the rest of us!"
