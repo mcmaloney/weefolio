a2634bce2d5d47c93dc72f42b59a5982a0a930a7
Michael Maloney <michaelmaloney@Crush-Air.local>
2010-04-18 00:22:35 -0400

    working on designs table redo- don't migrate yet...

--- db/migrate/20100416060915_redo_designs.rb
+++ db/migrate/20100416060915_redo_designs.rb
@@ -2,12 +2,20 @@ class RedoDesigns < ActiveRecord::Migration
   def self.up
     create_table :designs do |t|
       t.integer :user_id
-      t.integer :layout_type, :integer, :default => 1
-      t.header_image_file_name :string
-      t.header_image_content_type :string
-      t.header_image_file_size :integer
-      t.header_image_updated_at :datetime
-
+      t.integer :layout_type, :default => 1
+      t.string :header_image_file_name
+      t.string :header_image_content_type 
+      t.integer :header_image_file_size
+      t.datetime :header_image_updated_at
+      t.string :background_color, :default => "f5f5f3"
+      t.string :text_color, :default => "767574"
+      t.string :title_color, :default => "xxxxxx"
+      t.string :link_color, :default => "xxxxxx"
+      t.string :link_hover_color, :default => "242420"
+      t.string :header_color, :default => "xxxxxx"
+      t.string :font, :default => "Lucida Grande"
+      t.integer :font_size, :default => 11
+      
       t.timestamps
     end
   end
