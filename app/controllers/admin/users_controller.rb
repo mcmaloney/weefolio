fb963b05a65df651ddcdc5d16686b19f8dbdf44f
Michael Maloney <michael@michael-maloneys-imac.local>
2010-05-19 12:23:17 -0400

    making users admin better

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,4 +2,8 @@ class Admin::UsersController < AdminController
   def index
     @users = User.paginate :page => params[:page], :order => "created_at DESC"
   end
+  
+  def show
+    @user = User.find(params[:id])
+  end
 end
