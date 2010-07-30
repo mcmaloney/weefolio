375a6180364fdba564c64d10bad4114d67c50bcd
Michael Maloney <michaelmaloney@Crush-Air.local>
2010-07-29 21:02:35 -0400

    adding theme upload form

--- app/controllers/theme_uploads_controller.rb
+++ app/controllers/theme_uploads_controller.rb
@@ -1,5 +1,18 @@
 class ThemeUploadsController < ApplicationController
+  layout "template"
+  
   def new
+    @theme_upload = ThemeUpload.new(params[:theme_upload])
+  end
+  
+  def create
+    @theme_upload = ThemeUpload.new(params[:theme_upload])
+    if @theme_upload.save
+      redirect to themes_path
+      flash[:notice] = "Thanks for submitting a theme! We'll take a look and add it soon."
+    else
+      render :action => 'new'
+    end
   end
 
 end
