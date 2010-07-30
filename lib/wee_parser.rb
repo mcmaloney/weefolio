d7ca0cc250c1c5eba0cc260d264185d0254a633e
Michael Maloney <michaelmaloney@Crush-Air.local>
2010-07-29 22:55:21 -0400

    more avatar url stuff

--- lib/wee_parser.rb
+++ lib/wee_parser.rb
@@ -1,6 +1,6 @@
 module WeeParser
   CSS_DICT = ["color", "background", "font", "size", "text", "hover", "link", "title", "border", "header", "family"]
-  USER_DICT = ["header", "image", "contact", "form", "about", "me", "tagline", "rss", "url", "username"]
+  USER_DICT = ["header", "image", "contact", "form", "about", "me", "tagline", "rss", "url", "username", "avatar", "url"]
   PIECE_DICT = ["piece", "each", "pieces", "meta", "portfolio", "image", "gallery", "about", "client", "service", "for", "sale", "url", "if", "title"]
   GLOBAL_DICT = ["end", "javascript", "jquery", "wee", "script"]
   THRESH = 0.75
