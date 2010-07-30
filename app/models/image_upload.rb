79ce9375a123cc330c1d2323ea81d9cc92c7a557
Michael Maloney <michaelmaloney@Crush-Air.local>
2010-07-29 20:54:40 -0400

    bucket now specified in YAML file

--- app/models/image_upload.rb
+++ app/models/image_upload.rb
@@ -3,8 +3,7 @@ class ImageUpload < ActiveRecord::Base
   has_attached_file :image, :styles => { :small_thumb => "70x70#", :large_thumb => "116x116#", :full_size => "710x710>" },
                                             :storage => :s3, 
                                             :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
-                                            :path => ':attachment/:id/:style/:basename.:extension',
-                                            :bucket => 'weefolio'
+                                            :path => ':attachment/:id/:style/:basename.:extension'
                                             
                                             
   validates_presence_of :image
