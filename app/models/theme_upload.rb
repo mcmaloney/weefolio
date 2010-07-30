b807fd31c59b2f403cebb8c730fef002dd95d3c1
Michael Maloney <michaelmaloney@Crush-Air.local>
2010-07-29 20:56:03 -0400

    adding attachments to model

--- app/models/theme_upload.rb
+++ app/models/theme_upload.rb
@@ -1,2 +1,9 @@
 class ThemeUpload < ActiveRecord::Base
+  has_attached_file :screenshot, :styles => { :thumbnail => "150x230#" },
+                                            :storage => :s3, 
+                                            :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
+                                            :path => ':attachment/:id/:style/:basename.:extension'
+  has_attached_file :asset_package, :storage => :s3, 
+                                    :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
+                                    :path => ':attachment/:id/:style/:basename.:extension'
 end
